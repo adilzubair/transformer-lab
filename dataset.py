d5312d1aa79a4863af9b25ffe3d86f3d4998e28d
Muhamed Adil <muhamedadil03@gmail.com>
2025-12-11 14:51:31 +0400

    updates

--- dataset.py
+++ dataset.py
@@ -6,8 +6,8 @@ dataset = load_dataset("roneneldan/TinyStories", split="train", streaming=True)
 # 2. Extract text for training the tokenizer (take first 10,000 stories)
 # We need a raw text file to train the BPE tokenizer you planned
 iter_dataset = iter(dataset)
-with open("tinystories_train40.txt", "w", encoding="utf-8") as f:
-    for i in range(40000):
+with open("tinystories_train60.txt", "w", encoding="utf-8") as f:
+    for i in range(60000):
         data = next(iter_dataset)
         f.write(data['text'] + "\n") #type: ignore 
 
